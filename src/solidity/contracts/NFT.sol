6add8ab768486a15269c92f074c75da7ed9969e2
Luis Lucena <lucena@puller.mx>
2023-05-11 18:48:14 +0200

    add: scripts to deploy

--- src/solidity/contracts/NFT.sol
+++ src/solidity/contracts/NFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract NFT is ERC721URIStorage {
     uint public tokenCount;
 
-    constructor() ERC721("iOllin Token", "iON") {}
+    constructor() ERC721("iOllin Collection", "iON") {}
 
     function mint(string memory _tokenURI) external returns (uint) {
         tokenCount++;
---
+++ src/solidity/scripts/deploy.js
@@ -0,0 +1,23 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // Get the ContractFactories and Signers here.
+  const NFT = await ethers.getContractFactory("NFT");
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  // deploy contracts
+  const marketplace = await Marketplace.deploy(1);
+  const nft = await NFT.deploy();
+
+  console.log("Marketplace deployed to:", marketplace.address);
+  console.log("NFT deployed to:", nft.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
